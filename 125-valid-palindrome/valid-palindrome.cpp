cfe617763146ac8fe013518df1e999b3053c6835
Shubham <142610290+5hubham6@users.noreply.github.com>
2025-03-10 22:13:22 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (57.64%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(std::string s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !std::isalnum(s[left])) left++;
+            while (left < right && !std::isalnum(s[right])) right--;
+            if (std::tolower(s[left]) != std::tolower(s[right])) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
