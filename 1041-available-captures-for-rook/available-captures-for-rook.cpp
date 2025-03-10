a9e3be23c85c65d5f85c950dfc4a3a1390934750
Shubham <142610290+5hubham6@users.noreply.github.com>
2025-03-10 21:54:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (57.77%) - LeetSync

--- 1041-available-captures-for-rook/available-captures-for-rook.cpp
+++ 1041-available-captures-for-rook/available-captures-for-rook.cpp
@@ -1,25 +1,66 @@
 class Solution {
 public:
-  int numRookCaptures(vector<vector<char>>& board) {        
-    int ans = 0;
-    auto check = [&board](int x, int y, int dx, int dy) {
-      x += dx;
-      y += dy;
-      while (x >= 0 && x < 8 && y >= 0 && y < 8) {
-        if (board[y][x] == 'p') return 1;
-        if (board[y][x] != '.') break;
-        x += dx;
-        y += dy;
-      }
-      return 0;
-    };
-    
-    array<int, 5> dirs{1, 0, -1, 0, 1};
-    for (int i = 0; i < 8; ++i)
-      for (int j = 0; j < 8; ++j)
-        if (board[i][j] == 'R')
-          for (int d = 0; d < 4; ++d)          
-            ans += check(j, i, dirs[d], dirs[d + 1]);
-    return ans;
-  }
+    int numRookCaptures(std::vector<std::vector<char>>& board) {
+        int rookRow = -1;
+        int rookCol = -1;
+
+        // Find the rook's position
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                if (board[i][j] == 'R') {
+                    rookRow = i;
+                    rookCol = j;
+                    break;
+                }
+            }
+            if (rookRow != -1) {
+                break;
+            }
+        }
+
+        int captures = 0;
+
+        // Check in all four directions: up, down, left, right
+        // Up
+        for (int i = rookRow - 1; i >= 0; --i) {
+            if (board[i][rookCol] == 'p') {
+                captures++;
+                break;
+            } else if (board[i][rookCol] == 'B') {
+                break;
+            }
+        }
+
+        // Down
+        for (int i = rookRow + 1; i < 8; ++i) {
+            if (board[i][rookCol] == 'p') {
+                captures++;
+                break;
+            } else if (board[i][rookCol] == 'B') {
+                break;
+            }
+        }
+
+        // Left
+        for (int j = rookCol - 1; j >= 0; --j) {
+            if (board[rookRow][j] == 'p') {
+                captures++;
+                break;
+            } else if (board[rookRow][j] == 'B') {
+                break;
+            }
+        }
+
+        // Right
+        for (int j = rookCol + 1; j < 8; ++j) {
+            if (board[rookRow][j] == 'p') {
+                captures++;
+                break;
+            } else if (board[rookRow][j] == 'B') {
+                break;
+            }
+        }
+
+        return captures;
+    }
 };
