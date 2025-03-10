15dc044ae3961155e44a650019287fde256401a6
Shubham <142610290+5hubham6@users.noreply.github.com>
2025-03-10 22:23:55 +0530

    Time: 29 ms (68.86%) | Memory: 73.7 MB (36.24%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,17 +1,14 @@
+
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        {
-            for(int i = 1 ; i<nums.size();i++)
-            {
-                if(nums[i]==nums[i-1])
-                {
-                    return true;
-                }
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.find(num) != seen.end()) {
+                return true;
             }
-            return false;
+            seen.insert(num);
         }
-        
+        return false;
     }
 };
